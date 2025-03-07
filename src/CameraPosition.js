bcd2be00f5e636030e720c34227674619870fec3
EliasMN <elias010493@gmail.com>
2025-03-07 12:58:35 +0100

    Initial commit

---
+++ src/CameraPosition.js
@@ -0,0 +1,11 @@
+export const CAMERA_POSITIONS = [
+  { x: -65, y: 0, z: -700, rotationX: 0, rotationY: 1.85, rotationZ: 0 },
+  { x: -500, y: 0, z: -800, rotationX: 0, rotationY: 3, rotationZ: 0 },
+  { x: -700, y: 0, z: -500, rotationX: 0, rotationY: 4, rotationZ: 0 },
+  { x: -900, y: 0, z: -300, rotationX: 0, rotationY: 5, rotationZ: 0 },
+  { x: -800, y: 0, z: 450, rotationX: 0, rotationY: 5, rotationZ: 0 },
+  { x: -1100, y: -50, z: 200, rotationX: 0, rotationY: 5, rotationZ: 0 },
+  { x: -1500, y: 0, z: 450, rotationX: 0, rotationY: 5, rotationZ: 0 },
+  { x: -2500, y: 500, z: 450, rotationX: -1, rotationY: 5, rotationZ: -1 },
+  { x: -2800, y: 800, z: -800, rotationX: -2.5, rotationY: 5.5, rotationZ: -2.5 },
+];
---
+++ src/Scene.jsx
@@ -0,0 +1,61 @@
+import { useThree } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import { gsap } from "gsap";
+import { CAMERA_POSITIONS } from "./cameraPosition";
+
+function CameraController() {
+  const { camera } = useThree();
+  const currentPositionIndex = useRef(0);
+
+  function moveCamera(target) {
+    gsap.to(camera.position, {
+      x: target.x,
+      y: target.y,
+      z: target.z,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+    gsap.to(camera.rotation, {
+      x: target.rotationX,
+      y: target.rotationY,
+      z: target.rotationZ,
+      duration: 1,
+      ease: "power2.inOut",
+    });
+  }
+
+  useEffect(() => {
+    const handleScroll = (event) => {
+      if (event.deltaY > 0) {
+        currentPositionIndex.current = Math.min(
+          currentPositionIndex.current + 1,
+          CAMERA_POSITIONS.length - 1
+        );
+      } else {
+        currentPositionIndex.current = Math.max(
+          currentPositionIndex.current - 1,
+          0
+        );
+      }
+      moveCamera(CAMERA_POSITIONS[currentPositionIndex.current]);
+    };
+
+    window.addEventListener("wheel", handleScroll);
+    return () => window.removeEventListener("wheel", handleScroll);
+  }, []);
+
+  return null; // No renderiza nada, solo maneja la cámara
+}
+
+export function Scene() {
+  const fbx = useLoader(FBXLoader, "./Aula.fbx");
+
+  return (
+    <>
+      <primitive object={fbx} />
+      <CameraController />
+    </>
+  );
+}
